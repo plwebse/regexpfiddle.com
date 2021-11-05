70d0a98702351e98a12773e495c3f551b1cfd34c
plwebse <peter.lindblom@plweb.se>
2021-11-05 21:47:17 +0100

    moved to regexpfiddle.plweb.se

--- public_html/index.php
+++ public_html/index.php
@@ -170,44 +170,18 @@ function updateTitle(numberOfMatches){
 	}
 }
 
-function displayEmail(){
-	"use strict"
-	var mailElement = getElementById(mailId) 
-	
-	mailElement.href = "mailto:";
-	mailElement.href += "info";
-	mailElement.href += "@regexpfiddle";
-	mailElement.href += ".com";
 
-	mailElement.innerHTML += "info";
-	mailElement.innerHTML += "@regexpfiddle";
-	mailElement.innerHTML += ".com";
-	
-}
 
     </script>
-    <script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-25155117-1']);
-      _gaq.push(['_trackPageview']);
-      _gaq.push(['_trackPageLoadTime']);
-
-      (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
+    
   </head>
 <body onload="loadDataFromLocalStorage();displayEmail();">
 <form action="#donotdoit" method="get" onsubmit="return false;">
   <div class="main">
 
-    <h1 class="center">RegExpFiddle.com</h1>
+    <h1 class="center">RegExpFiddle.plweb.se</h1>
     <p class="center">
-      RegExpFiddle.com is a simple web service for you to try out some of your regular expression.
+      RegExpFiddle.plweb.se is a simple web service for you to try out some of your regular expression.
     </p>
 
     <div id="regexpContainer" class="container">
